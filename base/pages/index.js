498a4207ae291e203ff0f16b63c3b0598e186965
Leratech Solutions <mattia@leratechsolutions.com>
2021-11-08 14:37:16 +0100

    Added HelpDesk to Repo
    helpdesk task was missing, added to Repo.

--- base/pages/index.js
+++ base/pages/index.js
@@ -3,10 +3,16 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      <Link href="/">
-        <a>Hjem</a>
+      <h1>Henvendelser</h1>
+      <Link href="/news/[id]" as="/news/1">
+        <a>News 1</a>
       </Link>
-      <h1>Dette er hjem</h1>
+      <Link href="/news/[id]" as="/news/2">
+        <a>News 2</a>
+      </Link>
+      <Link href="/news/[id]" as="/news/3">
+        <a>News 3</a>
+      </Link>
     </div>
   )
 }
