850ed61ce32a9729c8457702ce2681c1e9eff84c
Leratech Solutions <mattia@leratechsolutions.com>
2021-11-15 20:44:22 +0100

    Added comment functions(notused)

--- helpdesk/pages/index.js
+++ helpdesk/pages/index.js
@@ -20,6 +20,39 @@ export default function Home() {
           },
           {
             id: 2,
+            title: 'Title one',
+            creator: 'Marius Wallin',
+            description:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In non neque diam. Nam placerat nunc id vulputate pretium. In ac metus sit amet augue suscipit ornare.',
+            department: 'salg',
+            severity: 'medium',
+            isResolved: false,
+            createdAt: new Date(2021, 11, 22).toLocaleDateString(),
+          },
+          {
+            id: 3,
+            title: 'Title one',
+            creator: 'Marius Wallin',
+            description:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In non neque diam. Nam placerat nunc id vulputate pretium. In ac metus sit amet augue suscipit ornare.',
+            department: 'salg',
+            severity: 'high',
+            isResolved: false,
+            createdAt: new Date(2021, 11, 22).toLocaleDateString(),
+          },
+          {
+            id: 4,
+            title: 'Title one',
+            creator: 'Marius Wallin',
+            description:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In non neque diam. Nam placerat nunc id vulputate pretium. In ac metus sit amet augue suscipit ornare.',
+            department: 'salg',
+            severity: 'low',
+            isResolved: false,
+            createdAt: new Date(2021, 11, 22).toLocaleDateString(),
+          },
+          {
+            id: 5,
             title: 'Title two',
             creator: 'Simen Simensen',
             description:
@@ -28,9 +61,24 @@ export default function Home() {
             severity: 'medium',
             isResolved: false,
             createdAt: new Date(2021, 11, 6).toLocaleDateString(),
+            comments: [
+              {
+                id: 1,
+                creator: 'Marius Wallin',
+                text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In non neque diam. Nam placerat nunc id vulputate pretium. In ac metus sit amet augue suscipit ornare.',
+                createdAt: new Date(2021, 11, 22).toLocaleDateString(),
+              },
+              {
+                id: 2,
+                title: 'Title three',
+                creator: 'Marius Wallin',
+                description:
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In non neque diam. Nam placerat nunc id vulputate pretium. In ac metus sit amet augue suscipit ornare.',
+              },
+            ],
           },
           {
-            id: 3,
+            id: 6,
             title: 'Title three',
             creator: 'Trude Trudesen',
             description:
