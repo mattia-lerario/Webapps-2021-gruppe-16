29cf7f7ad791d5ee38d63c969b5f8d00ca03a41a
Leratech Solutions <mattia@leratechsolutions.com>
2021-11-18 20:22:04 +0100

    Revert "create issue"
    This reverts commit 311ae62f9d80c96cb00c9d4b997db76bdc76cbef.

--- helpdesk/pages/api/issues/index.js
+++ helpdesk/pages/api/issues/index.js
@@ -1,6 +1,6 @@
 import prisma from '@/lib/clients/db'
 
-export default async function issues(req, res) {
+export default async function handler(req, res) {
   if (req.method === 'GET') {
     const issues = await prisma.issue.findMany({
       orderBy: {
@@ -8,26 +8,5 @@ export default async function issues(req, res) {
       },
     })
     return res.status(200).json({ success: true, issues })
-  } else if (req.method === 'POST') {
-    let department = await prisma.department.findUnique({
-      where: {
-        name: req?.body?.department,
-      },
-    })
-    let departmentId = department.id
-
-    const newIssue = await prisma.issue.create({
-      data: {
-        title: req?.body?.title,
-        creator: req?.body?.creator,
-        description: req?.body?.description,
-        severity: req?.body?.severity,
-        isResolved: false,
-        createdAt: new Date(),
-        departmentId: departmentId,
-      },
-    })
-
-    return res.status(200).json(newIssue)
   }
 }
