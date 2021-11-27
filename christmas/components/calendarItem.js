2ca78b7742d73cc6a61ca7ed4ed4ef4cadbe4da8
Leratech Solutions <mattia@leratechsolutions.com>
2021-11-27 13:08:06 +0100

    Added Calendar Grid, with Open buttons and color changes for Closed and Open.

--- christmas/components/calendarItem.js
+++ christmas/components/calendarItem.js
@@ -1,61 +1,69 @@
 import React from 'react'
 
-class CalendarItem extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      day: this.props.day,
-      dayOfWeek: this.props.dayOfWeek,
-      isSelected: this.props.isSelected,
-      isToday: this.props.isToday,
-      isHovered: false,
-      isActive: this.props.isActive,
-      isDisabled: this.props.isDisabled,
-      isSelectedMonth: this.props.isSelectedMonth,
-      isSelectedYear: this.props.isSelectedYear,
-    }
+const CalendarItem = () => {
+  //get all users
+  let cardColor = 'card-close'
+  const users = [
+    {
+      id: 1,
+      name: 'John Doe',
+    },
+  ]
+
+  const openCard = () => {
+    //open card and change cardColor to green
+    cardColor = 'card-open'
   }
 
-  //handle the hover event
-  handleHover() {
-    this.setState({
-      isHovered: !this.state.isHovered,
-    })
-  }
-
-  //handle the click event
-  handleClick() {
-    this.setState({
-      isSelected: !this.state.isSelected,
-    })
-  }
-
-  //handle the click event
-  handleActive() {
-    this.setState({
-      isActive: !this.state.isActive,
-    })
-  }
-
-  render() {
-    return (
-      <div
-        className={`calendar-item ${
-          this.state.isCurrentMonth ? 'current-month' : ''
-        } ${this.state.isSelected ? 'selected' : ''} ${
-          this.state.isToday ? 'today' : ''
-        } ${this.state.isHovered ? 'hovered' : ''} ${
-          this.state.isDisabled ? 'disabled' : ''
-        } ${this.state.isActive ? 'active' : ''} ${
-          this.state.isSelectedMonth ? 'selected-month' : ''
-        }
-        )`}
-        onClick={() => this.handleClick()}
-        onMouseEnter={() => this.handleHover()}
-        onMouseLeave={() => this.handleHover()}
-      >
-        {this.state.dayOfMonth}
-      </div>
-    )
-  }
+  // return a calendar item that loops 24 times and displays the current date
+  // and time in the format: MMM DD, YYYY hh:mm a
+  return (
+    <div className="calendar-ItemCard">
+      {[...Array(24)].map((e, i) => {
+        //set date to start first december of the year 2021
+        const date = new Date(2021, 11, 1, 0, 0, 0, 0)
+        date.setDate(date.getDate() + i)
+        return (
+          // return a calendar card box with the current date and time
+          <div className={cardColor} key={i}>
+            <div className="card-body">
+              <h5 className="card-title">
+                {date.toLocaleString('en-US', { month: 'long' })}{' '}
+                {date.getDate()}
+              </h5>
+              <p className="card-text">
+                {date.toLocaleString('en-US', {
+                  hour: 'numeric',
+                  minute: 'numeric',
+                  hour12: true,
+                })}
+              </p>
+              <div className="row">
+                <div className="col-6">
+                  <p className="card-text">
+                    {users.map((user) => {
+                      return (
+                        <div key={user.id}>
+                          <p>{user.name}</p>
+                        </div>
+                      )
+                    })}
+                  </p>
+                </div>
+                <div className="col-6">
+                  <p className="card-text">
+                    <button className="btn btn-primary" onClick={openCard()}>
+                      OPEN
+                    </button>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
+
+export default CalendarItem
