e109d45a65c56324ada431a2f18b2a2b90495124
Leratech Solutions <mattia@leratechsolutions.com>
2021-12-04 13:50:49 +0100

    Refactored array to be created from slots and calendar component

--- christmas/components/Slot.js
+++ christmas/components/Slot.js
@@ -1,8 +1,19 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 const Slot = (id, date, userInfo) => {
   const [isOpen, setIsOpen] = useState(true)
   let [cardStatus, setCardStatus] = useState('')
+
+  const [slots, setSlots] = useState([])
+
+  //generate slots array with 24 items
+  useEffect(() => {
+    let arr = []
+    for (let i = 0; i < 24; i++) {
+      arr.push(i)
+    }
+    setSlots(arr)
+  }, [])
   //get all users
 
   const users = [
@@ -31,7 +42,7 @@ const Slot = (id, date, userInfo) => {
     }
   }
 
-  //create function that returns a string with 4 random numbers and 4 random letters
+  //create function that returns a string with 4 random numbers and 4 random letters the code must be unique
   const generateCode = () => {
     let code = ''
 
@@ -44,10 +55,25 @@ const Slot = (id, date, userInfo) => {
     return code
   }
 
-  const createCard = () => {
+  const createCode = () => {
+    let code = generateCode()
+    //check if code is unique
+    if (
+      users.find(
+        (user) =>
+          user.cardSlots.find((slot) => slot.code === code) === undefined
+      )
+    ) {
+      return code
+    } else {
+      createCode()
+    }
+  }
+
+  const createCard = (id) => {
     //create card for user
     let date = new Date()
-    let code = generateCode()
+    let code = createCode()
     let newCard = {
       id: users[0].cardSlots.length + 1,
       date: date.toLocaleDateString(),
@@ -55,8 +81,7 @@ const Slot = (id, date, userInfo) => {
       code: code,
     }
     users[0].cardSlots.push(newCard)
-    openCard()
-    generateCode()
+    openCard(id)
     console.log(users[0].cardSlots)
   }
 
@@ -64,14 +89,19 @@ const Slot = (id, date, userInfo) => {
 
   return (
     <div className="calendar-Grid">
-      {[...Array(24)].map((e, i) => {
+      {slots.map((e, i) => {
         //set date to start first december of the year 2021
         const date = new Date(2021, 11, 1, 0, 0, 0, 0)
         date.setDate(date.getDate() + i)
         return (
           // return a calendar card box with the current date and time
-          <div className="card-body" key={i} id={i}>
-            <div className="card" onClick={createCard}>
+          <div
+            className={`card-body ${cardStatus}`}
+            onClick={createCard}
+            key={i}
+            id={i}
+          >
+            <div className="card">
               <h5 className="card-title date">
                 {date.toLocaleString('en-US', { month: 'long' })}{' '}
                 {date.getDate()}
--- christmas/components/calendar.js
+++ christmas/components/calendar.js
@@ -1,8 +1,10 @@
-import react from 'react'
+import react, { useEffect } from 'react'
 import { useStat } from 'react'
 
-const Calendar = (id, name,createdAt) => {
-  const {}
+const Calendar = (id, name, createdAt) => {
+  const [createdAt, setCreatedAt] = useState(createdAt)
+  const [name, setName] = useStat(name)
+  const [id, setId] = useStat(id)
 }
 
 export default Calendar
