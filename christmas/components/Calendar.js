2af982043d7f67296201822da30632ec664a7f45
Leratech Solutions <mattia@leratechsolutions.com>
2021-12-07 01:07:11 +0100

    merged rovik branch

--- christmas/components/Calendar.js
+++ christmas/components/Calendar.js
@@ -1,27 +1,36 @@
-/* eslint sort-imports: ["error", { "ignoreDeclarationSort": true }] */
-/* eslint no-console: ["error", { allow: ["warn", "error"] }] */
-
-import { useEffect, useState } from 'react'
-
+import React from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
-
+import { useUser } from '@/hooks/useUser'
+import { useRouter } from 'next/router'
 import Slot from './Slot'
+const Calendar = (date, userInfo) => {
+  //set slots data based on data from useEffect hook
+  const [slots, setSlots] = useState('')
+  const [status, setStatus] = useState('')
+  const [user, setUser] = useState('')
+  const router = useRouter()
+  const openCard = () => {
+    //open card for specific user and set status
 
-const Calendar = ({ calendar }) => {
-  const [slots, setSlots] = useState([])
+    setStatus('open')
+  }
+  //generate slots array with 24 items
 
   useEffect(() => {
     const fetch = async () => {
       await axios
-        .get(`/api/slots/${calendar?.id}`)
+        .get('/api/slots')
         .then((response) => {
-          setSlots(response.data)
+          const data = response.data
+          //stringify data to be able to use it in the slot component
+
+          setSlots(data)
         })
         .catch((error) => {
           console.warn(
-            `${error}\nMessage: Failed to fetch slots data for calendar id ${calendar?.id}`
+            `${error}\nMessage: Failed to fetch slots data for calendar id ${4}`
           )
-          setSlots([])
         })
     }
 
@@ -30,15 +39,94 @@ const Calendar = ({ calendar }) => {
     return () => {
       /* cleanup */
     }
-  }, [calendar.id])
+  }, [4])
+  /*useEffect(() => {
+    let arr = []
+    for (let i = 0; i < 24; i++) {
+      arr.push(i)
+    }
+    setSlots(arr)
+  }, [])*/
+  //get all users
+
+  const users = [
+    {
+      id: 1,
+      name: 'John Doe',
+      userSlots: [
+        {
+          id: 1,
+          date: '2020-12-12',
+          status: 'open',
+          code: 'abcd1234',
+        },
+      ],
+    },
+  ]
+
+  //function that returns a string with 4 random numbers and 4 random letters the code must be unique
+  const generateCode = () => {
+    let code = ''
+
+    for (let i = 0; i < 4; i++) {
+      code += String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+    }
+    for (let i = 0; i < 4; i++) {
+      code += Math.floor(Math.random() * 10)
+    }
+    return code
+  }
+
+  const createCode = () => {
+    let code = generateCode()
+    //check if code is unique
+    if (!user) {
+      return code
+    } else {
+      createCode()
+    }
+  }
+
+  const createUserSlot = async () => {
+    //create card for user
+    let date = new Date()
+    let code = createCode()
+    let userSlot = {
+      id: user.id,
+      date: date.toLocaleDateString(),
+      status: 'open',
+      code: code,
+    }
+
+    openCard()
+    console.log(users)
+  }
+
+  // return a calendar item that loops 24 times and displays the current date
 
   return (
-    <article className="calendar">
-      {slots &&
-        slots.map((element) => {
-          return <Slot key={element?.id} slot={element} />
-        })}
-    </article>
+    <div className="calendar-Grid">
+      {slots?.data?.map((slot) => {
+        //set date to start first december of the year 2021
+        console.log(slot)
+        const xid = JSON.stringify(slot)
+        console.log(xid)
+        return (
+          <div key={slot.id}>
+            <Slot
+              id={slot.id}
+              key={slot.id}
+              slug={slot.slug}
+              status={status}
+              createdAt={slot.createdAt}
+              openedAt={slot.openedAt}
+              calendarId={slot.calendarId}
+              userSlot={createUserSlot}
+            />
+          </div>
+        )
+      })}
+    </div>
   )
 }
 
