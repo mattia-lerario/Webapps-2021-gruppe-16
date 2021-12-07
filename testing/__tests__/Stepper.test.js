4f19b503e8a28f6769ab1aaf70e1321c3f095c80
Ole Germann Kristensen <69955297+olegkhiof@users.noreply.github.com>
2021-12-07 17:23:26 +0100

    installed jest plus 1st test done

--- testing/__tests__/Stepper.test.js
+++ testing/__tests__/Stepper.test.js
@@ -2,9 +2,18 @@
  * @jest-environment jsdom
  */
 
+import {render, screen} from '@testing-library/react'
+import {Stepper} from '@/components/Stepper.js'
+
 describe('Stepper component', () => {
-  it('should render button', () => {})
-  it('should have correct text content on button', () => {})
+  render(<Stepper/>)
+  const button = document.getElementsByTagName('button');
+  it('should render button', () => {
+    expect(button).toContainHTML;
+  })
+  it('should have correct text content on button', () => {
+
+  })
 
   it('should update step-count and button content on click', async () => {})
   it('should remove button when step count is higher than amount of steps', async () => {})
